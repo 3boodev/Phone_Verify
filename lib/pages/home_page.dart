d17c72a33e6b432564190d74ca1b951eae6e9d49
3boodev <abdalla33360@gmail.com>
2022-02-17 01:11:23 +0200

    commit message

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,14 +19,6 @@ class _HomePageState extends State<HomePage> {
   TextEditingController num_controller=TextEditingController();
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    final data = Provider.of<PhoneData>(context, listen: false);
-    data.fetchData(context);
-    num_controller = TextEditingController(text: data.number);
-  }
-  @override
   void dispose() {
     num_controller.dispose();
     super.dispose();
@@ -36,53 +28,57 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final data = Provider.of<PhoneData>(context);
 
-    return Scaffold(
-      appBar: AppBar(title: const Text("Phone Validate App"),centerTitle: true,),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 40,bottom: 20,left: 250,right: 250),
-              child:Column(
-                children: [
-                  TextField(
-                    controller: num_controller,
-                    keyboardType: TextInputType.number,
-                    onChanged: data.setnumber(num_controller.text),
-                    decoration: const InputDecoration(
-                      labelText:"Enter Your Number",
-                      prefixIcon: Icon(Icons.phone),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(height: 30,),
-                  ElevatedButton(onPressed: ()
-                    {
-                      data.setnumber(num_controller.text);
-                    },
-                    child:const Text("Verify Number",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),),)
-                ],
-              )
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('The Phone is Valid',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,),),
-                Text('Mobile Number : ${data.verifyModel.number}'),
-                Text('Local Format  : ${data.verifyModel.localFormat}'),
-                Text('International Format : ${data.verifyModel.internationalFormat}'),
-                Text('Country Prefix : ${data.verifyModel.countryPrefix}'),
-                Text('Country Code : ${data.verifyModel.countryCode}'),
-                Text('Country Name : ${data.verifyModel.countryName}'),
-                Text('Location : ${data.verifyModel.location}'),
-                Text('Carrier : ${data.verifyModel.carrier}'),
-                Text('LineType : ${data.verifyModel.lineType}'),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+    return ChangeNotifierProvider(create: (context)=>PhoneData(),
+    child:
+    Scaffold(
+    appBar: AppBar(title: const Text("Phone Validate App"),centerTitle: true,),
+    body: Center(
+    child: Column(
+    children: [
+    Padding(
+    padding: const EdgeInsets.only(top: 40,bottom: 20,left: 250,right: 250),
+    child:Column(
+    children: [
+    TextField(
+    controller: num_controller,
+    keyboardType: TextInputType.number,
+    onChanged: data.setnumber(num_controller.text),
+    decoration: const InputDecoration(
+    labelText:"Enter Your Number",
+    prefixIcon: Icon(Icons.phone),
+    border: OutlineInputBorder(),
+    ),
+    ),
+    const SizedBox(height: 30,),
+    ElevatedButton(onPressed: ()
+    {
+      data.setnumber(num_controller.text);
+      data.fetchData(context);
+    },
+    child:const Text("Verify Number",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),),)
+    ],
+    )
+    ),
+    Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    Text(data.verifyModel.valid==true?'The Phone is Valid':'The Phone is Not Valid',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,),),
+    Text('Mobile Number : ${data.verifyModel.number}'),
+    Text('Local Format  : ${data.verifyModel.localFormat}'),
+    Text('International Format : ${data.verifyModel.internationalFormat}'),
+    Text('Country Prefix : ${data.verifyModel.countryPrefix}'),
+    Text('Country Code : ${data.verifyModel.countryCode}'),
+    Text('Country Name : ${data.verifyModel.countryName}'),
+    Text('Location : ${data.verifyModel.location}'),
+    Text('Carrier : ${data.verifyModel.carrier}'),
+    Text('LineType : ${data.verifyModel.lineType}'),
+    ],
+    )
+    ],
+    ),
+    ),
+    )
+    ,);
   }
 }
 
